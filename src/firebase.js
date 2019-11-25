13b6e7f47792e0ceece69f9fde773f92c2a1bc8d
hyunsukimsokcho <hskimsokcho@gmail.com>
2019-11-25 18:39:05 +0900

    [backend-firebase] Add config for firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+import 'firebase/storage';
+import config from './config';
+
+// firebase initialize
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL);  // Auto-login for default (o.w. SESSION)
+
+export const database = firebase.database();
+export default firebase;
